030d67b12efb10c79ae8fc86e0201be85323e47d
Dmitry <dmitrykira@ya.ru>
2024-06-14 13:20:52 +0400

    add home work 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello java");
+
+        /*System.out.println("***************************");
+        System.out.println("****      Визитка      ****");
+        System.out.println("****   Name: Дмитрий   ****");
+        System.out.println("****   Skype: Kira163  ****");
+        System.out.println("***************************");*/
+
+        System.out.println("***************************");
+        System.out.println("****      Визитка      ****");
+        System.out.println("****   Name: \"Дмитрий\" ****");
+        System.out.println("****   Skype: Kira163  ****");
+        System.out.println("***************************");
+
+
+    }
+}
